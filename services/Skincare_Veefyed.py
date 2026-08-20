d8af2ef3945046a524523c1f355d192ea5edbcbf
Olajide Shittu <74042061+olajideshittu@users.noreply.github.com>
2026-08-20 09:52:08 +0100

    Fix skincare scraper import syntax

--- services/Skincare_Veefyed.py
+++ services/Skincare_Veefyed.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import pandas as pd\
+import pandas as pd
 import time
 
 def scrape_skincare_products(
